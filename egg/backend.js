7abfa0da64e015346346de44fcbc45144988158d
Maxfojtik <maxfojtik@bex.net>
2021-01-15 18:55:22 -0500

    Update backend.js

--- egg/backend.js
+++ egg/backend.js
@@ -1,6 +1,6 @@
 
 // var wsUri = "ws://localhost:12389"; // Localhost
-var wsUri = "wss://74.140.3.27:8765"; // Max's
+var wsUri = "ws://74.140.3.27:8765"; // Max's
 function ordinal_suffix_of(i) {
     var j = i % 10,
         k = i % 100;
